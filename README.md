44ddd5dca16865b4698af9af0e396d6e8a08f738
Ilya Grigorik <ilya@igvita.com>
2012-03-11 15:25:24 -0700

    import archiver

---
+++ README.md
@@ -0,0 +1,55 @@
+# GitHub Archive
+
+[http://www.githubarchive.org]()
+
+Open-source developers all over the world are working on millions of projects: writing code & documentation, fixing & submitting bugs, and so forth. GitHub Archive is a project to **record** the public GitHub timeline, **archive it**, and **make it easily accessible for** further analysis.
+
+![Stats](http://www.stathat.com/graphs/1d/97/2c57ce23e5bfcd3d5ffc822b587f.png)
+
+----
+
+GitHub provides 17 event types, which range from new commits and fork events, to opening new tickets, commenting, and adding members to a project. The activity is aggregated in hourly archives, which you can access with any HTTP client:
+
+<table>
+<thead>
+  <tr>
+    <th>Query</th>
+    <th>Command</th>
+  </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>Activity for March 03, 2012 at 3PM PST</td>
+    <td><code>wget http://data.githubarchive.org/2012-03-11-15.json.gz</code></td>
+  </tr>
+  <tr>
+    <td>Activity for March 03, 2012</td>
+    <td><code>wget http://data.githubarchive.org/2012-03-11-{0..24}.json.gz</code></td>
+  </tr>
+  <tr>
+    <td>Activity for March 2012</td>
+    <td><code>wget http://data.githubarchive.org/2012-03-{0..30}-{0..24}.json.gz</code></td>
+  </tr>
+</tbody>
+</table>
+
+Each archive contains a stream of JSON encoded GitHub events ([sample](https://gist.github.com/2017462), which you can process in any language. Ruby example:
+
+```ruby
+require 'open-uri'
+require 'zlib'
+require 'yajl'
+
+gz = open('http://data.githubarchive.org/2012-03-11-12.json.gz')
+js = Zlib::GzipReader.new(gz).read
+
+Yajl::Parser.parse(js) do |event|
+  print event
+end
+```
+
+__Note: timeline data is available starting March 11, 2012.__
+
+### License
+
+(MIT License) - Copyright (c) 2012 Ilya Grigorik
---
+++ crawler/crawler.rb
@@ -0,0 +1,96 @@
+require 'log4r'
+require 'yajl'
+
+require 'em-http'
+require 'em-http/middleware/json_response'
+require 'em-stathat'
+
+include EM
+
+##
+## Setup
+##
+
+#HttpRequest.use Middleware::JSONResponse
+StatHat.config do |c|
+  c.ukey  = ENV['STATHATKEY']
+  c.email = 'ilya@igvita.com'
+end
+
+@log = Log4r::Logger.new('github')
+@log.add(Log4r::StdoutOutputter.new('console', {
+  :formatter => Log4r::PatternFormatter.new(:pattern => "[#{Process.pid}:%l] %d :: %m")
+}))
+
+@latest = []
+
+##
+## Crawler
+##
+
+EM.run do
+  stop = Proc.new do
+    puts "Terminating crawler"
+    EM.stop
+  end
+
+  Signal.trap("INT",  &stop)
+  Signal.trap("TERM", &stop)
+
+  @file_generator = lambda { File.new("data/#{Time.now.strftime('%Y-%m-%d-%k')}.json.current", "a+") }
+  @file = @file_generator.call
+
+  # set the hourly timer to fire on the hour
+  EM.add_timer(60*60 - (Time.now.to_i % (60*60))) do
+    rotate = Proc.new do
+      @log.info "Rotating file: #{@file.to_path}"
+      @file.close
+
+      File.rename(@file.to_path, @file.to_path.chomp('.current'))
+      @file = @file_generator.call
+    end
+
+    rotate.call
+
+    # swap the file every 60 minutes after that
+    EM.add_periodic_timer(60*60, &rotate)
+  end
+
+  process = Proc.new do
+      req = HttpRequest.new("https://github.com/timeline.json").get({
+      :head => {
+        'user-agent' => 'archiver.io'
+      }
+    })
+
+    req.callback do
+      begin
+        latest = Yajl::Parser.parse(req.response)
+        urls = latest.collect {|e| e['url']}
+        new_events = latest.reject {|e| @latest.include? e['url']}
+
+        @latest = urls
+        new_events.each do |event|
+          @file.write(Yajl::Encoder.encode(event))
+        end
+
+        @log.info "Found #{new_events.size} new events"
+        StatHat.new.ez_count('Github Events', new_events.size)
+
+        if new_events.size >= 25
+          EM.add_timer(1.5, &process)
+        end
+
+      rescue Exception => e
+        @log.error "Processing exception: #{e}, #{e.backtrace.first(5)}"
+	@log.error "Response: #{req.response_header}, #{req.response}"
+      end
+    end
+
+    req.errback do
+      @log.error "Error: #{req.response_header.status}, header: #{req.response_header}, response: #{req.response}"
+    end
+  end
+
+  EM.add_periodic_timer(6, &process)
+end
