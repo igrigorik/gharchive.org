34e3bafc205cc4c7bb7e31df97604dae2e110e35
Ilya Grigorik <ilya@igvita.com>
2012-04-04 21:39:11 -0700

    refactor transform to just focus on the csv flatten

--- crawler/bigquery/transform.rb
+++ crawler/bigquery/transform.rb
@@ -1,8 +1,34 @@
-require 'tempfile'
+require 'optparse'
 require 'time'
 require 'zlib'
 require 'yajl'
 require 'csv'
+
+options = {schema: 'schema.js', verbose: false}
+OptionParser.new do |opts|
+  opts.banner = "Usage: flatten.rb [options]"
+
+  opts.on("-i", "--input FILE", "input filename") do |v|
+    options[:input] = v
+  end
+
+  opts.on("-o", "--output FILE", "output filename") do |v|
+    options[:output] = v
+  end
+
+  opts.on("-s", "--schema FILE", "schema file") do |v|
+    options[:schema] = v
+  end
+
+  opts.on("-v", "--verbose", "verbose log") do |v|
+    options[:verbose] = true
+  end
+end.parse!
+
+#
+# Map GitHub JSON schema to flat CSV space based
+# on provided Big Query column schema
+#
 
 def flatmap(h, e, prefix = '')
   e.each do |k,v|
@@ -15,38 +41,31 @@ def flatmap(h, e, prefix = '')
   h
 end
 
-input = ARGV.shift
-if input.nil?
-  puts "No input file specified"
-  exit(1)
-end
-
-schema = Yajl::Parser.parse(open('schema.js').read)
+start = Time.now
+schema = Yajl::Parser.parse(open(options[:schema]).read)
 headers = schema['configuration']['load']['schema']['fields'].map {|f| f['name']}
 
 begin
-  tmp = Tempfile.new("bigquery-#{input}")
-  js  = Zlib::GzipReader.new(open(input)).read
+  out = File.new(options[:output] || (options[:input] + "-out.csv"), "w")
+  js  = Zlib::GzipReader.new(open(options[:input])).read
+  cnt = 0
 
   Yajl::Parser.parse(js) do |event|
-  	r = CSV::Row.new(headers, [])
-  	flatmap({}, event).each do |k,v|
+    r = CSV::Row.new(headers, [])
+    flatmap({}, event).each do |k,v|
       v = (Time.parse(v).utc.strftime('%Y-%m-%d %T') rescue '') if k =~ /_at$/
       if r.include? k
         r[k] = v
       else
-        puts "Unknown field: #{k}, value: #{v}"
+        puts "Unknown field: #{k}, value: #{v}" if options[:verbose]
       end
     end
-  	tmp << r.to_s
+
+    cnt += 1
+    out << r.to_s
   end
 
-  puts "Uploading file to BigQuery"
-  puts system('python upload.py ' + tmp.path)
-
-  puts "\nDone.\n"
-
+  puts "Processed #{options[:input]}: #{cnt} rows in #{(Time.now - start).round} seconds"
 ensure
-  tmp.close
-  tmp.unlink
+  out.close
 end
