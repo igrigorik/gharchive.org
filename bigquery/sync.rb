b96b810260c1bbdc5643eca399baf8503d3408ac
Ilya Grigorik <ilya@igvita.com>
2012-04-20 11:19:15 -0700

    force token refresh to avoid race condition

--- bigquery/sync.rb
+++ bigquery/sync.rb
@@ -1,7 +1,10 @@
 require 'optparse'
 require 'time'
+require 'json'
 
-options = {verbose: false, sync: true}
+ARGV << '--help' if ARGV.empty?
+
+options = {verbose: false, sync: true, token: '.bigquery.v2.token'}
 OptionParser.new do |opts|
   opts.banner = "Usage: sync.rb [options]"
 
@@ -11,6 +14,10 @@ OptionParser.new do |opts|
 
   opts.on("-s", "--[no-]sync", "sync upload") do |v|
     options[:sync] = v
+  end
+
+  opts.on("-t", "--token FILE", "token filename") do |v|
+    options[:token] = v
   end
 
   opts.on("-v", "--verbose", "verbose log") do |v|
@@ -24,9 +31,18 @@ raise OptionParser::MissingArgument if options[:file].nil?
 r = system("ruby transform.rb -i #{options[:file]} -o /tmp/bq.csv #{'-v' if options[:verbose]}")
 exit(1) if !r
 
+# expire current oauth token
+j = JSON.parse(IO.read(options[:token]))
+j['access_token'] = 'expired'
+
+# force refresh of the current token
+File.open(options[:token], 'w') {|f| f.write JSON.generate(j)}
+system("/usr/local/bin/bq --credential_file /home/archiver/githubarchive/bigquery/.bigquery.v2.token ls")
+
 # upload the data to BigQuery
 system("/usr/local/bin/bq #{'--nosync' if !options[:sync]} " +
        "#{'--apilog true' if options[:verbose]} " +
+       "--credential_file /home/archiver/githubarchive/bigquery/.bigquery.v2.token " +
        "load github.events /tmp/bq.csv.gz")
 
 File.unlink("/tmp/bq.csv.gz")
