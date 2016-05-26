ff00f6eb514abbbe091c9e0c4cda179b8aa1af46
Ilya Grigorik <ilya@igvita.com>
2016-05-26 15:21:46 -0700

    decrease interval

--- crawler/crawler.rb
+++ crawler/crawler.rb
@@ -83,7 +83,7 @@ EM.run do
         @log.error "Processing exception: #{e}, #{e.backtrace.first(5)}"
         @log.error "Response: #{req.response_header}, #{req.response}"
       ensure
-        EM.add_timer(2.0, &process)
+        EM.add_timer(1.5, &process)
       end
     end
 
@@ -92,7 +92,7 @@ EM.run do
                   header: #{req.response_header}, \
                   response: #{req.response}"
 
-      EM.add_timer(2.0, &process)
+      EM.add_timer(1.5, &process)
     end
   end
 
